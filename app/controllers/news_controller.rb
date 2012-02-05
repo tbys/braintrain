441269cb6cfe361ffa51b4acb4ba8ef570e67873
tbys <kapipi@gmail.com>
2012-02-05 18:57:50 +0900

    First commit

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,3 @@
+class NewsController < ActionController::Base
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Braintrain2::Application.config.secret_token = '1f6edc6fb2a501519b826b57a8ec228a294ac54bf9157700f1fbf977c2b887956b7a5bed2dddd3287c27d9caec56d6e78cf65a0e9bac3d53ad64ea1a5a6302d2'
